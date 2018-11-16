e79743b472d1119f8da90e3775263002002f8bf4
Tim Webster <twebster17@gmail.com>
2018-11-15 17:36:38 -0700

    write Calc_het_vcf.py

---
+++ scripts/Calc_het_vcf.py
@@ -0,0 +1,115 @@
+from __future__ import print_function
+import argparse
+import collections
+import gzip
+import statistics
+import pandas as pd
+
+
+def parse_args():
+	parser = argparse.ArgumentParser()
+
+	parser.add_argument(
+		"--vcf", required=True,
+		help="")
+
+	parser.add_argument(
+		"--min_sites", required=True, type="int",
+		help="")
+
+	parser.add_argument(
+		"--min_ind", required=True, type="int",
+		help="")
+
+	parser.add_argument(
+		"--output_file", required=True,
+		help="Path to and name of output file")
+
+	args = parser.parse_args()
+
+	return args
+
+
+def main():
+	args = parse_args()
+	dict1 = collections.OrderedDict()
+	sample_dict = collections.OrderedDict()
+
+	with gzip.open(args.vcf, 'rt', encoding='utf-8') as f:
+		for line in f:
+			# Ignore header
+			if line[:2] == "##":
+				continue
+			record = line.split()
+			if record[0] == "#CHROM":
+				for idx, i in enumerate(record[9:]):
+					sample_dict[str(idx)] = i
+
+			chrom = record[0]
+			if chrom not in dict1:
+				dict1[chrom] = []
+				for i in len(sample_dict):
+					dict1[chrom].append([0, 0])
+
+			# Looking for biallelic snps
+			if len(record[3]) != 1:
+				continue
+			alts = record[4].split(",")
+			alts = [x for x in alts if x != "<NON_REF>"]
+			if len(alts) not in [1, 2]:
+				continue
+			lengths = [len(x) for x in alts]
+			if len(lengths) == 0:
+				continue
+			if all(x == 1 for x in lengths) is False:
+				continue
+
+			for idx, i in enumerate(record[9:]):
+				format = i.split(":")
+				gt = format[0]
+				if gt == "./.":
+					continue
+				dict1[chrom][idx][0] += 1
+				if gt[0] != gt[2]:
+					dict1[chrom][idx][1] += 1
+
+	sex_dict = collections.OrderedDict()
+	with open(args.sexes, "r") as f:
+		for line in f:
+			line_split = line.strip().split()
+			if len(line_split) != 2:
+				continue
+			sex_dict[line_split[0]] = line_split[1]
+
+	for i in sample_dict:
+		sample_dict[i] = sex_dict[sample_dict[i]]
+
+	results_dict = collections.OrderedDict()
+	for i in dict1:
+		male_vals = []
+		fem_vals = []
+
+		for idx, k in enumerate(dict1[i]):
+			sex = sex_dict[str(idx)]
+			if k[0] >= args.min_sites:
+				if sex == "male":
+					male_vals.append(float(k[1]) / k[0])
+				elif sex == "female":
+					fem_vals.append(float(k[1]) / k[0])
+				else:
+					raise ValueError("Sexes need to be the strings 'male' or 'female'.")
+
+		if male_vals >= args.min_ind and fem_vals >= args.min_ind:
+			results_dict[i] = [i, statistics.mean(male_vals), statistics.mean(fem_vals)]
+		else:
+			results_dict[i] = [i, -1, -1]
+
+	df = pd.DataFrame.from_dict(results_dict, orient='index')
+	df.columns = ["chrom", "male_het_rate", "female_het_rate"]
+	print(df)
+	df = df.sort_values(["chrom"])
+	df.to_csv(args.output_file, index=False)
+
+
+if __name__ == "__main__":
+	main()
