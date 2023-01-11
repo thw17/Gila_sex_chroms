01a56ed515b4b1ecee4e1109b1a2149c01148f96
Tim Webster <twebster17@gmail.com>
2023-01-10 23:45:16 -0700

    use dict comprehension

--- scripts/Filter_result_file_for_ortho_gila_gallus.py
+++ scripts/Filter_result_file_for_ortho_gila_gallus.py
@@ -40,7 +40,8 @@ with open(ortho, "r") as f:
 for k in gff2_gff1_lookup:
 	if gff2_gff1_lookup[k] in gff1_mult:
 		gff2_mult.append(k)
-		del gff2_gff1_lookup[k]
+
+gff2_gff1_lookup_final = {key: val for key, val in gff2_gff1_lookup if key not in gff2_mult}
 
 print("Multiple hits in gff1: {}".format(gff1_mult))
 print("")
@@ -55,11 +56,11 @@ with open(gff2_result, "r") as g:
 		start = split[2]
 		male = split[3]
 		female = split[4]
-		if (chrom, start) in gff2_gff1_lookup:
-			gff2_in_gff1[gff2_gff1_lookup[(chrom, start)]] = (male, female)
+		if (chrom, start) in gff2_gff1_lookup_final:
+			gff2_in_gff1[gff2_gff1_lookup_final[(chrom, start)]] = (male, female)
 
 print("gff1_coords", len(gff1_coords))
-print("gff2_gff1_lookup", len(gff2_gff1_lookup))
+print("gff2_gff1_lookup_final", len(gff2_gff1_lookup_final))
 print("gff2_in_gff1", len(gff2_in_gff1))
 
 with open(outfile, "w") as o:
